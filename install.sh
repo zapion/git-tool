3173c532aef9ce349f180176fd856f49905aad30
Paul Yang <paulyang@kkstream.com>
2021-11-08 18:14:27 +0800

    # commit format: # fix/feat: [ticket-1] desc.. # ci/chore: no ticket needed
    fix: [ticket-2] desc..

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ HOOK_FILE=.git/hooks/prepare-commit-msg
 
 if [ -d .git ]
 then
+    mkdir -p .git/hooks
     curl -o $HOOK_FILE https://raw.githubusercontent.com/zapion/git-tool/main/prepare-commit-msg
     chmod +x $HOOK_FILE
 else
