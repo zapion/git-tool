8681d1d6cc204c635e208d945b14b92c8a300b69
Paul Yang <paulyang@kkstream.com>
2021-11-08 17:43:29 +0800

    fix/feat: [ticket-1] desc..

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+HOOK_FILE=.git/hooks/prepare-commit-msg
 
 if [ -d .git ]
 then
-    curl -o https://raw.githubusercontent.com/zapion/git-tool/main/prepare-commit-msg .git/hooks/
+    curl -o $HOOK_FILE https://raw.githubusercontent.com/zapion/git-tool/main/prepare-commit-msg
+    chmod +x $HOOK_FILE
 else
     echo "This is not a git repo"
     exit 1
